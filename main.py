c03be9abe09a64c23cedfcd1da6589d236662c51
jsegoviaburillo <jsegovia@unizar.es>
2024-07-02 18:32:29 +0200

    Working version for i, i+1 discretization dx dependance should be adressed, waves not observed

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 from lib.canal import *
 import os
 
-if __name__ == '__main__':
-    
-    # Clear files
-    os.system('rm -rf img/*')
-  
-    # river = Canal()
+if __name__ == "__main__":
 
-    # river.calc_vectors()
-    # river.temporal_loop(mode='wave') # Can be 'flux' or 'wave' for fluxes or contributions scheme 
-    
+    # Clear files
+    os.system("rm -rf img/*")
+
     river = Canal()
 
-    river.temporal_loop(mode='flux') # Can be 'flux' or 'wave' for fluxes or contributions scheme     
-    
+    river.temporal_loop(
+        mode="wave"
+    )  # Can be 'flux' or 'wave' for fluxes or contributions scheme
+
     # river.prog_bar.next()
     river.prog_bar.finish()
+
+    river.save_config()
